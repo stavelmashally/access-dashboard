ba76629387082844178616602949988e5eff96c2
stavelmashally <stavelmashally6@gmail.com>
2021-06-10 16:13:20 +0300

    loading indicator

--- mockServer/node_modules/mocking_g/mg_client/src/tree/actions/editor.js
+++
@@ -1,9 +0,0 @@
-import { get } from "plugins/requests";
-
-export function generate(focus,amount) {
-    const library = focus.lib;
-    const category = focus.cat;
-    amount = amount || 1;
-
-    return get("/generate",{library,category,amount});
-}
