8d4533ecd54a09ab884c9ad695f77a200e16f3ed
stavelmashally <stavelmashally6@gmail.com>
2021-07-11 10:27:54 +0300

    new build

--- client/src/plugins/settings/api/index.js
+++ client/src/plugins/settings/api/index.js
@@ -3,15 +3,11 @@ import axios from 'axios';
 const BASE_URL = 'http://localhost:5050';
 
 const fetchDefaultConfig = () => {
-  return axios.get(
-    `${BASE_URL}/defaultConfig`
-  );
+  return axios.get(`${BASE_URL}/defaultConfig`);
 };
 
 const fetchModifyConfig = () => {
-  return axios.get(
-    `${BASE_URL}/modifyConfig`
-  );
+  return axios.get(`${BASE_URL}/modifyConfig`);
 };
 
 export { fetchDefaultConfig, fetchModifyConfig };
